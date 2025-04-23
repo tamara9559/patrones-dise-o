c27e05ebc15c168dd1cee313a389535edc5d6967
ingsoftManuel <132092799+ingsoftManuel@users.noreply.github.com>
2025-04-22 21:54:01 -0500

    Delete factorymethod directory

--- factorymethod/src/EnvioAereo.java
+++
@@ -1,6 +0,0 @@
-public class EnvioAereo implements Envio {
-    @Override
-    public String realizarEntrega() {
-        return "Entrega realizada por avión (envío aéreo).";
-    }
-}
--- factorymethod/src/EnvioMaritimo.java
+++
@@ -1,6 +0,0 @@
-public class EnvioMaritimo implements Envio {
-    @Override
-    public String realizarEntrega() {
-        return "Entrega realizada por barco (envío marítimo).";
-    }
-}
