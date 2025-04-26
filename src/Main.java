39f0905d65bd0e6cfc4f0f1014965d3246fd9aaf
Juan Manuel <juanm20061@gmail.com>
2025-04-25 19:54:37 -0500

    Agrega ejemplo del patrón adapter en Java

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+// Interfaz que el sistema espera (Target)
+interface HDMI {
+    void conectarConHDMI();
+}
+
+// Clase existente con una interfaz incompatible (Adaptee VGA)
+class VGA {
+    public void conectarConVGA() {
+        System.out.println("Conectado usando interfaz VGA.");
+    }
+}
+
+// Clase nueva con una interfaz incompatible (Adaptee DisplayPort)
+class DisplayPort {
+    public void conectarConDisplayPort() {
+        System.out.println("Conectado usando interfaz DisplayPort.");
+    }
+}
+
+// Adaptador que permite usar un VGA como si fuera HDMI
+class AdaptadorVGAaHDMI implements HDMI {
+    private VGA vga;
+
+    public AdaptadorVGAaHDMI(VGA vga) {
+        this.vga = vga;
+    }
+
+    @Override
+    public void conectarConHDMI() {
+        System.out.println("Adaptando señal de VGA a HDMI...");
+        vga.conectarConVGA();
+    }
+}
+
+// Adaptador que permite usar un DisplayPort como si fuera HDMI
+class AdaptadorDisplayPortaHDMI implements HDMI {
+    private DisplayPort displayPort;
+
+    public AdaptadorDisplayPortaHDMI(DisplayPort displayPort) {
+        this.displayPort = displayPort;
+    }
+
+    @Override
+    public void conectarConHDMI() {
+        System.out.println("Adaptando señal de DisplayPort a HDMI...");
+        displayPort.conectarConDisplayPort();
+    }
+}
+
+// Cliente que solo trabaja con HDMI
+class Cliente {
+    public static void main(String[] args) {
+        // Dispositivo con salida VGA
+        VGA miMonitorVGA = new VGA();
+        HDMI adaptadorVGA = new AdaptadorVGAaHDMI(miMonitorVGA);
+        adaptadorVGA.conectarConHDMI();
+
+        // Dispositivo con salida DisplayPort
+        DisplayPort miMonitorDP = new DisplayPort();
+        HDMI adaptadorDP = new AdaptadorDisplayPortaHDMI(miMonitorDP);
+        adaptadorDP.conectarConHDMI();
+    }
+}
